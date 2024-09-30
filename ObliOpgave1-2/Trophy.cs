b495b1d6c893bec379a2abbfd9cf1cffde92880c
Victor Tadeusz Ulstrup Olszowski <Victor.olszowski@gmail.com>
2024-09-30 10:56:29 +0200

    Add project files.

---
+++ ObliOpgave1-2/Trophy.cs
@@ -0,0 +1,43 @@
+﻿namespace ObliOpgave1_2
+{
+    namespace ObligatoriskOpg1_2
+    {
+        public class Trophy
+        {
+            public int Id { get; set; }
+            public string Competition { get; set; }
+            public int Year { get; set; }
+
+            public Trophy() { }
+
+            public override string ToString()
+            {
+                return $"Id: {Id}, Competition: {Competition}, Year: {Year}";
+            }
+
+            public void ValidateComp()
+            {
+                if (Competition == null || Competition.Trim().Length < 3)
+                {
+                    throw new ArgumentOutOfRangeException("Competition navn skal være min 3 bogstaver langt");
+                }
+            }
+            public void ValidateYear()
+            {
+                if (Year < 1970 || Year > 2024)
+                {
+                    throw new ArgumentOutOfRangeException("Året skal ligge mellem 1970 og 2024");
+                }
+            }
+
+            public void Validate()
+            {
+                ValidateYear();
+                ValidateComp();
+            }
+
+
+        }
+    }
+
+}
---
+++ ObliOpgave1-2Tests/TrophyTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObliOpgave1_2.ObligatoriskOpg1_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObliOpgave1_2.ObligatoriskOpg1_2.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        [TestMethod()]
+        public void TrophyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateCompTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateYearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
